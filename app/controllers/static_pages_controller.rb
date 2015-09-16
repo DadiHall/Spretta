61018c3054628c4ccaaad4ef0e508b98431c3ce1
Dadi Hall <dadih32@gmail.com>
2015-09-16 09:45:06 +0000

    Finish layout and routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,4 +14,7 @@ class StaticPagesController < ApplicationController
     def disclaimer
     end
     
+    def contact
+    end
+    
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -30,5 +30,11 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
         assert_select "title", "Spretta Socially Green"
   end
+    
+    test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Spretta Socially Green"
+  end
 
 end
