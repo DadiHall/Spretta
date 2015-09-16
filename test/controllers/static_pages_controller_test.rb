403658c7d4adf2292a8afd797f7ff7d987585dd7
Dadi Hall <dadih32@gmail.com>
2015-09-16 01:34:55 +0000

    Add a full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,26 +4,31 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+      assert_select "title", "Spretta Socially Green"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+      assert_select "title", "Spretta Socially Green"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+      assert_select "title", "Spretta Socially Green"
   end
 
   test "should get policy" do
     get :policy
     assert_response :success
+      assert_select "title", "Spretta Socially Green"
   end
     
     test "should get disclaimer" do
         get :disclaimer
     assert_response :success
+        assert_select "title", "Spretta Socially Green"
   end
 
 end
