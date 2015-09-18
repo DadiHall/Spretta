613beb5f577b2504d50b496ae45f2e407e48a8e5
Dadi Hall <dadih32@gmail.com>
2015-09-18 11:02:53 +0000

    Finisdh log in / log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
     def create
     @user = User.new(user_params)
     if @user.save
+        log_in @user
         flash[:success] = "Velkomin í Sprettu, þú sérð ekki eftir því"
       redirect_to @user
     else
